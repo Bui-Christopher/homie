a619f9192fd786660ccab7d3b3a4a4b1b289e9ec
Chris <cbui@crownsterling.io>
2024-04-14 12:54:54 -0700

    Add datasets to gitignore and improve workflow

--- homie-api/src/main.rs
+++ homie-api/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// TODO: Delete
+// Notes for later:
+// let tmp: Regions = regions
+//     .counties
+//     .into_iter()
+//     .filter(|region| region.city() == "IRVINE")
+//     .collect();
