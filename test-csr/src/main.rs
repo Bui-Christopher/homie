382e350e894a388bdea38fba18bfea99b6913145
Chris <cbui@crownsterling.io>
2024-05-22 23:45:14 -0700

    Continue leptos book

--- test-csr/src/main.rs
+++ test-csr/src/main.rs
@@ -3,5 +3,28 @@ use leptos::*;
 fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+
+    leptos::mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button on:click=move |_| { set_count.update(|n| *n += 10); }>
+            "Click me: "
+        </button>
+        <ProgressBar progress=count/>
+    }
+}
+#[component]
+fn ProgressBar(progress: ReadSignal<i32>) -> impl IntoView {
+    view! {
+        <progress
+            max="50"
+            // now this works
+            value=progress
+        />
+    }
 }
