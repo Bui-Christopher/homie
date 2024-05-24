132a6a2f9016104f36c788349986c6f7eef8a860
Chris <cbui@crownsterling.io>
2024-05-24 16:56:10 -0700

    Reactive lists leptos book

--- test-csr/src/main.rs
+++ test-csr/src/main.rs
@@ -9,21 +9,37 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
+    let length = 10;
+    let counters = (1..=length).map(|idx| create_signal(idx));
+
+    // each item manages a reactive view
+    // but the list itself will never change
+    let counter_buttons = counters
+        .map(|(count, set_count)| {
+            view! {
+                <li>
+                    <button
+                        on:click=move |_| set_count.update(|n| *n += 1)
+                    >
+                        {count}
+                    </button>
+                </li>
+            }
+        })
+        .collect_view();
 
     view! {
-        <button on:click=move |_| { set_count.update(|n| *n += 10); }>
-            "Click me: "
-        </button>
-        <ProgressBar progress=count/>
+        <ul>{counter_buttons}</ul>
     }
 }
 #[component]
-fn ProgressBar(progress: ReadSignal<i32>) -> impl IntoView {
+fn ProgressBar(
+    #[prop(default = 50)] max: u16,
+    #[prop(into)] progress: Signal<i32>,
+) -> impl IntoView {
     view! {
         <progress
-            max="50"
-            // now this works
+            max=max
             value=progress
         />
     }
